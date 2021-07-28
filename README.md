ce17a5f1d1dd90aa4d0ecc6b49c5a09b7c820c73
Bryan R <77365758+br-pki@users.noreply.github.com>
2021-07-27 22:00:08 -0400

    implement stratified sample

--- README.md
+++ README.md
@@ -6,14 +6,16 @@
  1. language_mappings.csv
      Simple file to faciliate mappings between the English name for a language and the ISO 639-1 (2-digit) and the ISO 639-3 (3-digit) code. 
  1. create_Tatoeba_train_test.py
-    usage: create_Tatoeba_train_test.py [-h] [-l [LANGUAGES ...]] [-m MINIMUM_SENTENCES]
+    usage: create_Tatoeba_train_test.py [-h] [--languages [LANGUAGES ...]] [--minimum_sentences MINIMUM_SENTENCES] [--sample_type {random,stratify}]
 
     optional arguments:
       -h, --help            show this help message and exit
-      -l [LANGUAGES ...], --languages [LANGUAGES ...]
-                            languages to include in output          
-      -m MINIMUM_SENTENCES, --minimum_sentences MINIMUM_SENTENCES
+      --languages [LANGUAGES ...]
+                            languages to include in output
+      --minimum_sentences MINIMUM_SENTENCES
                             minimum number of sentences found in corpus
+      --sample_type {random,stratify}
+                            type of sample to take: "random" or "stratify"
                         
      You can limit the Tatoeba sentences based on language and/or number of sentences per language in the corpus.
      The default languages are English, Chinese, German, Spanish, French, Italian, Japanese, Korean, Portuguese, Danish, Dutch, and Norwegian. Any subset of those can be specified using the --languages flag.
--- create_Tatoeba_train_test.py
+++ create_Tatoeba_train_test.py
@@ -2,7 +2,10 @@
 import argparse
 import tarfile
 import urllib.request
+import re
 import pandas as pd
+import warnings
+warnings.filterwarnings('ignore')
 from datetime import datetime
 from sklearn.model_selection import train_test_split
 
@@ -70,9 +73,34 @@ def get_sentences(min_sentences: int, these_languages: list) -> pd.DataFrame:
     language_filter = language_counts[(language_counts["Count"]>=min_sentences)&(~language_counts["English Name"].isna())]["ISO 639-3 Name"].tolist()
     result = sentences[sentences["Language"].isin(language_filter)]
     if result.empty: print("No sentences found for given language/minimum sentence criteria...")
+    else: print(language_filter)
+    # TODO update print out about actual languages included in sample
+    
     return result
 
-def take_sample(these_sentences: pd.DataFrame) -> pd.DataFrame:
+def get_sentence_word_char_len(this_row: pd.Series) -> int:
+    """
+    Count the number of words or characters in a sentence based
+    on the language of the sentence.
+    
+    Args:
+    this_row - pd.Series, a row from a pd.DataFrame.
+    
+    Returns:
+    result - int; the count of words or characters in the sentence.
+    """
+    assert isinstance(this_row, pd.Series), "this_row is not of type pd.Series."
+    this_language = this_row["Language"]
+    this_text = this_row["Sentence"]
+    # remove punctuation from this_text
+    this_text = re.sub(r"[。？?！!，.“”、「」]", "", this_text) # the pattern is just a number of punctuations I found in the Asian texts
+    if this_language in ["Chinese", "Japanese", "Korean"]:
+        result = len(this_text)
+    else:
+        result = len(this_text.split())
+    return result
+
+def take_sample(these_sentences: pd.DataFrame, sample_type: str) -> pd.DataFrame:
     """
     Take a random sample from a DataFrame containing Tatoeba sentences
     with each language contributing no more than the minimum number
@@ -80,17 +108,43 @@ def take_sample(these_sentences: pd.DataFrame) -> pd.DataFrame:
 
     Args:
     these_sentences - pd.DataFrame; sentences & their assigned language.
+    sample_type - str; the type of sample to take: "random" or "stratify"
 
     Returns:
     this_sample - pd.DataFrame; a subset of sentences.
     """
     assert isinstance(these_sentences, pd.DataFrame), "these_sentences is not of type pd.DataFrame."
     assert not these_sentences.empty, "these_sentences is an empty DataFrame."
-    print("Taking a sample...")
-    # TODO: implement a better way of limiting total sample size while considering the size of individual languges in subset of corpus so train-test isn't huge
-    max_sample_size = int(these_sentences["Language"].value_counts().min() / 1000) * 1000
-    print(f"...of {max_sample_size} sentences per language.")
-    this_sample = these_sentences.groupby("Language", group_keys=False).apply(lambda x: x.sample(max_sample_size))
+    assert isinstance(sample_type, str), "sample_type is not of type str."
+    assert sample_type in ["random", "stratify"], 'sample_type is not "random" or "stratify"'
+    print(f"Taking a {'stratified' if sample_type=='stratify' else 'random'} sample...")
+    
+    if sample_type == "random":
+        # TODO: implement a better way of limiting total sample size while considering the size of individual languges in subset of corpus so train-test isn't huge
+        max_sample_size = int(these_sentences["Language"].value_counts().min() / 1000) * 1000
+        print(f"...of {max_sample_size} sentences per language.")
+        this_sample = these_sentences.groupby("Language", group_keys=False).apply(lambda x: x.sample(max_sample_size))
+    elif sample_type == "stratify":
+        # call get_sentence_word_char_len
+        these_sentences["Sentence_word_char_len"] = these_sentences.apply(lambda row: get_sentence_word_char_len(row), axis=1)
+        # only keep sentences with <201 chars
+        these_sentences = these_sentences[these_sentences["Sentence_word_char_len"]<100]
+        # bin sentences by len
+        bin_labels = ["1","2","3","4","5","6","7","8","9","10","11 to 16", "17 to 27", "28 to 48", "49 to 99"]
+        these_sentences["bin"] = pd.cut(
+            x=these_sentences["Sentence_word_char_len"],
+            bins=[0,1,2,3,4,5,6,7,8,9,10,16,27,48,99],
+            labels=bin_labels
+        )
+        # iterate through bins & take sample for that bin
+        this_sample = pd.DataFrame()
+        for this_bin in bin_labels:
+            temp_sample = these_sentences[these_sentences["bin"]==this_bin]
+            # find max sample size
+            max_sample_size = temp_sample["Language"].value_counts(normalize=False).min()
+            print(f"...of {max_sample_size} sentences per language for bin = {this_bin}.")
+            temp_sample = temp_sample.groupby("Language", group_keys=False).apply(lambda x: x.sample(max_sample_size))
+            this_sample = this_sample.append(temp_sample)
     return this_sample
 
 def main():
@@ -100,7 +154,7 @@ def main():
     # TODO: implement option to specify where to save test-train (if outputted)
     # TODO: implement option to specify train size: float between 0 & 1
     parser.add_argument(
-        "-l", "--languages",
+        "--languages",
         nargs="*",
         default = [
             "English",
@@ -120,10 +174,19 @@ def main():
         required=False
     )
     parser.add_argument(
-        "-m", "--minimum_sentences",
+        "--minimum_sentences",
         type=int,
         default=5000,
-        help="minimum number of sentences found in corpus"
+        help="minimum number of sentences found in corpus",
+        required=False
+    )
+    parser.add_argument(
+        "--sample_type",
+        type=str,
+        default="random",
+        choices=["random", "stratify"],
+        help='type of sample to take: "random" or "stratify"',
+        required=False
     )
     args = parser.parse_args()
     if args.languages:
@@ -132,7 +195,7 @@ def main():
     # get sentences, take sample, create train & test
     sentences = get_sentences(min_sentences=args.minimum_sentences, these_languages=args.languages)
     if not sentences.empty:
-        sample = take_sample(sentences)
+        sample = take_sample(sentences, args.sample_type)
         y = sample["Language"]
         X_train, X_test, y_train, y_test = train_test_split(
             sample["Sentence"],
@@ -141,7 +204,6 @@ def main():
             shuffle=True,
             stratify=y
         )
-        # TODO: implement option for stratifying by sentence length as well
         # combine train & test into DFs & save each in output/
         train = X_train.to_frame().merge(right=y_train, how="inner", left_index=True, right_index=True)
         test = X_test.to_frame().merge(right=y_test, how="inner", left_index=True, right_index=True)
@@ -149,8 +211,8 @@ def main():
         # save train & test files
         now = datetime.now()
         datetime_stamp = f"{now.year}-{now.month}-{now.day}_{now.hour}{now.minute}"
-        train.to_csv(f"output/Tatoeba_train_{datetime_stamp}.csv", index_label="Original Index")
-        test.to_csv(f"output/Tatoeba_test_{datetime_stamp}.csv", index_label="Original Index")
+        train.to_csv(f"output/Tatoeba_{args.sample_type}_train_{datetime_stamp}.csv", index_label="Original Index")
+        test.to_csv(f"output/Tatoeba_{args.sample_type}_test_{datetime_stamp}.csv", index_label="Original Index")
         print("Train & test files saved to output/")
     else:
         print("No sample generated.")
